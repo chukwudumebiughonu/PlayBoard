3a16fa395b7aa1a556ea266fb83f1f4dfd56c0d3
chukwudumebiughonu <plasdexcel14@gmail.com>
2024-02-12 18:11:24 +0100

    adding tool-button and toolbar

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,12 +1,86 @@
-export const Toolbar = () => {
+import { 
+    Circle, 
+    MousePointer2, 
+    Pencil, 
+    Redo2, 
+    Square, 
+    StickyNote, 
+    Type,
+    Undo2
+  } from "lucide-react";
+  
+  import { ToolButton } from "./tool-button";
+  
+  interface ToolbarProps {
+    canvasState: CanvasState;
+    setCanvasState: (newState: CanvasState) => void;
+    undo: () => void;
+    redo: () => void;
+    canUndo: boolean;
+    canRedo: boolean;
+  };
+  
+  export const Toolbar = ({
+    canvasState,
+    setCanvasState,
+    undo,
+    redo,
+    canUndo,
+    canRedo,
+  }: ToolbarProps) => {
     return (
-        <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
-            Toolbar!
+      <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
+        <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
+          <ToolButton
+            label="Select"
+            icon={MousePointer2}
+            onClick={() => {}}
+          />
+          <ToolButton
+            label="Text"
+            icon={Type}
+            onClick={() => {}}
+          />
+          <ToolButton
+            label="Sticky note"
+            icon={StickyNote}
+            onClick={() => {}}
+          />
+          <ToolButton
+            label="Rectangle"
+            icon={Square}
+            onClick={() => {}}
+          />
+          <ToolButton
+            label="Ellipse"
+            icon={Circle}
+            onClick={() => {}}
+          />
+          <ToolButton
+            label="Pen"
+            icon={Pencil}
+            onClick={() => {}}
+          />
         </div>
-    )
-}
-
-export const ToolbarSkeleton = () => {
+        <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
+          <ToolButton
+            label="Undo"
+            icon={Undo2}
+            onClick={undo}
+            isDisabled={!canUndo}
+          />
+          <ToolButton
+            label="Redo"
+            icon={Redo2}
+            onClick={redo}
+            isDisabled={!canRedo}
+          />
+        </div>
+      </div>
+    );
+  };
+  
+  export const ToolbarSkeleton = () => {
     return (
       <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 bg-white h-[360px] w-[52px] shadow-md rounded-md" />
     );
