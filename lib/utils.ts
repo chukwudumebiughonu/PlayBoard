601bc6b8f2abe38b28d4adf62f4ffc5a1b05bf95
chukwudumebiughonu <plasdexcel14@gmail.com>
2024-02-13 09:13:43 +0100

    complete cursor presence

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,16 +1,16 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-// import { 
-//   Camera, 
-//   Color, 
-//   Layer, 
-//   LayerType, 
-//   PathLayer, 
-//   Point, 
-//   Side, 
-//   XYWH
-// } from "@/types/canvas";
+import { 
+  Camera, 
+  Color, 
+  Layer, 
+  LayerType, 
+  PathLayer, 
+  Point, 
+  Side, 
+  XYWH
+} from "@/types/canvas";
 
 const COLORS = [
   "#DC2626", 
@@ -28,15 +28,15 @@ export function connectionIdToColor(connectionId: number): string {
   return COLORS[connectionId % COLORS.length];
 };
 
-// export function pointerEventToCanvasPoint(
-//   e: React.PointerEvent,
-//   camera: Camera,
-// ) {
-//   return {
-//     x: Math.round(e.clientX) - camera.x,
-//     y: Math.round(e.clientY) - camera.y,
-//   };
-// };
+export function pointerEventToCanvasPoint(
+  e: React.PointerEvent,
+  camera: Camera,
+) {
+  return {
+    x: Math.round(e.clientX) - camera.x,
+    y: Math.round(e.clientY) - camera.y,
+  };
+};
 
 // export function colorToCss(color: Color) {
 //   return `#${color.r.toString(16).padStart(2, "0")}${color.g.toString(16).padStart(2, "0")}${color.b.toString(16).padStart(2, "0")}`;
--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -6,7 +6,7 @@ import {
 } from "@liveblocks/client";
 import { createRoomContext } from "@liveblocks/react";
 
-// import { Layer, Color } from "@/types/canvas";
+import { Layer, Color } from "@/types/canvas";
   
 const client = createClient({
   throttle: 16,
@@ -20,17 +20,17 @@ type Presence = {
   cursor: { x: number, y: number } | null,
   selection: string[];
   pencilDraft: [x: number, y: number, pressure: number][] | null;
-  // penColor: Color | null;
+  penColor: Color | null;
 };
 
 // Optionally, Storage represents the shared document that persists in the
 // Room, even after all users leave. Fields under Storage typically are
 // LiveList, LiveMap, LiveObject instances, for which updates are
 // automatically persisted and synced to all connected clients.
-// type Storage = {
-//   layers: LiveMap<string, LiveObject<Layer>>;
-//   layerIds: LiveList<string>;
-// };
+type Storage = {
+  layers: LiveMap<string, LiveObject<Layer>>;
+  layerIds: LiveList<string>;
+};
 
 // Optionally, UserMeta represents static/readonly metadata on each user, as
 // provided by your own custom auth back end (if used). Useful for data that
